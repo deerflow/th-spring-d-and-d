a2222f90248206a9e7f91899c8bd23915572b071
Florian <alu.florian@gmail.com>
2021-08-25 15:36:39 +0200

    refacto: Change use of DAO to use API instead

--- src/main/java/com/deerflow/thspringdandd/App.java
+++ src/main/java/com/deerflow/thspringdandd/App.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
+    public final static String apiUrl = "http://localhost:8080";
 
-	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 
 }
--- src/main/java/com/deerflow/thspringdandd/characters/CharacterController.java
+++ src/main/java/com/deerflow/thspringdandd/characters/CharacterController.java
@@ -1,15 +1,27 @@
 package com.deerflow.thspringdandd.characters;
 
+import com.deerflow.thspringdandd.App;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class CharacterController {
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
     @GetMapping("/characters")
-    public String charactersList(Model model) {
-        model.addAttribute("characters", Characters.findAll());
+
+    public String charactersList(Model model, RestTemplate restTemplate) {
+        ResponseEntity<Character[]> characters = restTemplate.getForEntity(App.apiUrl + "/characters", Character[].class);
+        model.addAttribute("characters", characters.getBody());
         return "charactersList";
     }
 
@@ -20,32 +32,34 @@ public class CharacterController {
     }
 
     @PostMapping("/characters/new")
-    public RedirectView createCharacter(@ModelAttribute Character character) {
-        Characters.createOne(character);
+    public RedirectView createCharacter(@ModelAttribute Character character, RestTemplate restTemplate) {
+        restTemplate.postForObject(App.apiUrl + "/characters", character, Boolean.class);
         return new RedirectView("/characters");
     }
 
     @DeleteMapping("/characters/delete/{id}")
-    public RedirectView deleteCharacter(@PathVariable(value = "id") int id) {
-        Characters.deleteOne(id);
+    public RedirectView deleteCharacter(@PathVariable(value = "id") int id, RestTemplate restTemplate) {
+        restTemplate.delete(App.apiUrl + "/characters/" + id);
         return new RedirectView("/characters");
     }
 
     @GetMapping("/characters/edit/{id}")
-    public String editCharacterForm(@PathVariable(value = "id") int id, Model model) {
-        model.addAttribute("character", Characters.findById(id));
+    public String editCharacterForm(@PathVariable(value = "id") int id, Model model, RestTemplate restTemplate) {
+        Character character = restTemplate.getForObject(App.apiUrl + "/characters/" + id, Character.class);
+        model.addAttribute("character", character);
         return "editCharacterForm";
     }
 
     @PutMapping("/characters/edit/{id}")
-    public RedirectView editCharacter(@ModelAttribute Character character, @PathVariable(value = "id") int id) {
-        Characters.editOne(id, character);
+    public RedirectView editCharacter(@ModelAttribute Character character, @PathVariable(value = "id") int id, RestTemplate restTemplate) {
+        restTemplate.put(App.apiUrl + "/characters/" + id, character);
         return new RedirectView("/characters/" + character.getId());
     }
 
     @GetMapping("/characters/{id}")
-    public String getCharacterById(Model model, @PathVariable(value = "id") int id) {
-        model.addAttribute("character", Characters.findById(id));
+    public String getCharacterById(Model model, @PathVariable(value = "id") int id, RestTemplate restTemplate) {
+        Character character = restTemplate.getForObject(App.apiUrl + "/characters/" + id, Character.class);
+        model.addAttribute("character", character);
         return "oneCharacter";
     }
 }
